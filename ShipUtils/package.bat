61527ee7f09ec48c11ddbfc17de19998579ecda2
NBT22 <noah.thwing@gmail.com>
2024-02-22 23:35:54 -0600

    Cleanup

--- ShipUtils/package.bat
+++ ShipUtils/package.bat
@@ -1,4 +1,4 @@
 cd "../../../Package Files"
 del package.zip
-powershell -command "$json = Get-Content .\manifest.json -raw | ConvertFrom-Json;$json.version_number=([Xml](Get-Content ..\ShipUtils.csproj)).Project.PropertyGroup.Version;$json | ConvertTo-Json | Set-Content .\manifest.json"
+powershell -command "$json = Get-Content .\manifest.json -raw | ConvertFrom-Json;$json.version_number=([Xml](Get-Content ..\ShipUtils.csproj)).Project.PropertyGroup.Version[0];$json | ConvertTo-Json | Set-Content .\manifest.json"
 tar -a -c -f package.zip icon.png README.md manifest.json
